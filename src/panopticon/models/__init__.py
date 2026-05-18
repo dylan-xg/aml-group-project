658646764e69722989d96ace2bc059b6b78d3c0e
dylan-xg <117338067+dylan-xg@users.noreply.github.com>
2026-05-18 17:52:19 +1000

    Added models folder
    This folder is where the modules that load the machine learning models and provide access to them will be.

---
+++ src/panopticon/models/__init__.py
@@ -0,0 +1,2 @@
+# Within this folder, create a module (python file) each each type of model we will use.
+# We will need to discuss design decisions regarding how the model should be loaded, stored, and called for inference.
