ce2ba9559cd48c2f5c088f1ef0ff945065d269df
dylan-xg <117338067+dylan-xg@users.noreply.github.com>
2026-05-24 01:19:19 +1000

    Drafting up a deepface module
    Not sure if we will continue this going forward. Will need to check.

---
+++ src/panopticon/recognition/_deepface.py
@@ -0,0 +1,43 @@
+"""Integrates the models into DeepFace."""
+
+#from deepface import DeepFace
+from deepface.modules import preprocessing, modeling
+from deepface.models.FacialRecognition import FacialRecognition
+#from deepface.models.facial_recognition.Facenet import load_facenet128d_model
+from deepface.modules.verification import thresholds, confidences
+
+from ._models import BaseModel
+
+
+def add_model_to_deepface(model: BaseModel) -> None:
+	class NewModelClient(FacialRecognition):
+		def __init__(self) -> None:
+			self.model = model.embedding_model
+			self.model_name = model.name
+			self.input_shape = self.model.input_shape[1:3]
+			self.output_shape = self.model.output_shape[-1]
+			#type(self.model)
+
+	# Adding your model name to the avaliable model list in the right category, facial recognition.
+	modeling.AVAILABLE_MODELS["facial_recognition"][model.name] = NewModelClient
+
+	# Just here because I cloned facenet to test the code.
+	# Format: (variable) thresholds: dict[str, Any]
+	thresholds[model.name] = thresholds["Facenet"]
+	# Format: (variable) confidences: dict[str, dict[str, dict[str, float]]]
+	confidences[model.name] = confidences["Facenet"]
+
+def custom_normalisation():
+	original_normalise_input = preprocessing.normalize_input
+
+	def custom_normalise_input(img, normalisation="base"):
+		if normalisation == 'model.name': # Yeah I know, will be changed
+			# --- Custom normalisation logic, change for each model. ---
+			mean, std = img.mean(), img.std()
+			img = (img - mean) / std
+			# --- Do anything you want within these comments. ---
+			return img
+
+		return original_normalise_input(img=img, normalization=normalisation)
+
+	preprocessing.normalize_input = custom_normalise_input
