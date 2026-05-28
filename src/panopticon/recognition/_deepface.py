fcb5476f151c391676e5d9094380d3e41db6fd2d
creator9832742983 <coreywilliams1906@gmail.com>
2026-05-29 09:54:06 +1000

    Changes _model.py & _deepface.py
    Expands the model class in _model.py to accept needed variables, path to .keras, then helps get the other variables, confs, thresholds from it. Also you can put specific normalization behavior in there. _deepface changes are so it can be accepted + deepface.facialrecognition.forward() gets fixed within it, if wrong, so it's more lenient.

--- src/panopticon/recognition/_deepface.py
+++ src/panopticon/recognition/_deepface.py
@@ -9,8 +9,10 @@ from deepface.models.FacialRecognition import FacialRecognition
 #from deepface.models.facial_recognition.Facenet import load_facenet128d_model
 from deepface.modules.verification import thresholds, confidences
 
+import numpy as np
 
-def add_model_to_deepface(model) -> None:
+
+def add_example_model_to_deepface(model) -> None:
 	class NewModelClient(FacialRecognition):
 		def __init__(self) -> None:
 			self.model = model.embedding_model
@@ -28,10 +30,10 @@ def add_model_to_deepface(model) -> None:
 	# Format: (variable) confidences: dict[str, dict[str, dict[str, float]]]
 	confidences[model.name] = confidences["Facenet"]
 
-def custom_normalisation():
+def example_custom_normalisation():
 	original_normalise_input = preprocessing.normalize_input
 
-	def custom_normalise_input(img, normalisation="base"):
+	def example_custom_normalise_input(img, normalisation="base"):
 		if normalisation == 'model.name': # Yeah I know, will be changed
 			# --- Custom normalisation logic, change for each model. ---
 			mean, std = img.mean(), img.std()
@@ -41,4 +43,52 @@ def custom_normalisation():
 
 		return original_normalise_input(img=img, normalization=normalisation)
 
-	preprocessing.normalize_input = custom_normalise_input
+	preprocessing.normalize_input = example_custom_normalise_input
+
+
+def add_model_to_deepface(model) -> None:
+	class NewModelClient(FacialRecognition):
+		def __init__(self) -> None:
+			self.model = model.embedding_model
+			self.model_name = model.name
+			self.input_shape = model.input_shape
+			self.output_shape = model.output_shape
+
+		def forward(self, img):
+
+			if img.ndim == 3:
+				img = np.expand_dims(img, axis=0)
+
+			if img.ndim != 4:
+				raise ValueError(f"Input image must be shaped like (batch, X, X, 3), but got {img.shape}")
+
+			embeddings = self.model(img, training=False)
+
+			if hasattr(embeddings, "numpy"):
+				embeddings = embeddings.numpy()
+
+
+			if embeddings.shape[0] == 1:
+				return embeddings[0].tolist()
+
+			return embeddings.tolist()
+
+	modeling.AVAILABLE_MODELS["facial_recognition"][model.name] = NewModelClient
+
+	thresholds[model.name] = model.thresholds
+	confidences.update(model.confidences)
+
+
+def custom_normalisation(model) -> None:
+	original_normalize_input = preprocessing.normalize_input
+
+	def custom_normalize_input(img, normalization="base"):
+		if normalization == model.normalization:
+			return model.normalize(img)
+
+		return original_normalize_input(
+			img=img,
+			normalization=normalization
+		)
+
+	preprocessing.normalize_input = custom_normalize_input
