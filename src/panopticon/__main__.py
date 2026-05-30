3a8a30ee604a442a4dc94cfeb29f380ab30dfeb4
dylan-xg <117338067+dylan-xg@users.noreply.github.com>
2026-05-30 13:39:16 +1000

    Executing add to deepface

--- src/panopticon/__main__.py
+++ src/panopticon/__main__.py
@@ -1,11 +1,17 @@
 """The entry point for panopticon."""
 
 from .modules import LOADED_MODULES
-from .recognition import detect_in_frame
+from .recognition import (
+	CustomClassifierEmbeddingModel,
+	add_model_to_deepface,
+	detect_in_frame,
+)
 from .settings import SETTINGS
 from .ui import UserInterface
 from .video_feed import VideoFeed
 
+
+add_model_to_deepface(CustomClassifierEmbeddingModel())
 
 video_feed = VideoFeed(
 	capture_location=SETTINGS.input_source(),
--- src/panopticon/recognition/__init__.py
+++ src/panopticon/recognition/__init__.py
@@ -4,5 +4,9 @@ from ._comparison import (
 	compare_faces as compare_faces,
 	compare_faces_from_path as compare_faces_from_path,
 )
-from ._model import ExampleModel as ExampleModel
+from ._deepface import add_model_to_deepface as add_model_to_deepface
+from ._model import (
+	CustomClassifierEmbeddingModel as CustomClassifierEmbeddingModel,
+	ExampleModel as ExampleModel,
+)
 from ._run import detect_in_frame as detect_in_frame
